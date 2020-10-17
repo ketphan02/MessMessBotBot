5fa4927aa60d822037b00e9664b3d901eb22ff98
PhanKiet <tuankietvn@outlook.com>
2020-10-17 15:18:11 +0700

    Update

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-import { WEBHOOK } from './webhook';
+import WEBHOOK from './webhook';
+import PERSISTENT_MENU from './utils/persistent_menu';
 
-WEBHOOK();
+async function __main__()
+{
+    await PERSISTENT_MENU();
+    
+    await WEBHOOK();
+}
+
+__main__();
--- src/webhook.ts
+++ src/webhook.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import verifyWebhook from './verifyWebhook/verify';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,37 +10,17 @@ const port = process.env.PORT || 3000;
 /**
  * @description Initialize the server and handle all the requests.
  */
-const WEBHOOK = () =>
+const WEBHOOK = async () =>
 {
     app.use(bodyParser.json());
     app.use(express.urlencoded({ extended: false}));
 
-    app.get("/", (req, res) =>
-    {
-        res.send("Bruh");
-    });
+    verifyWebhook(app);
 
-    app.get('/verifyWebhook', (req, res) =>
-    {
-        const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-        const mode = req.query['hub.mode'];
-        const token = req.query['hub.verify_token'];
-        const challenge = req.query['hub.challenge'];
-
-        if (mode && token)
-        {
-            if (mode === 'subscribe' && token === VERIFY_TOKEN)
-            {
-                console.log('VERIFIED');
-                res.status(200).send(challenge);
-            }
-            else res.sendStatus(403);
-        }
-    });
 
     // Start the server
     app.listen(port, () => console.log(`Listening on ${port}`));
 }
 
-export { WEBHOOK };
+export default WEBHOOK;
