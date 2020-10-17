3b1073e5572055ef5a197e55ff639d160da93270
PhanKiet <tuankietvn@outlook.com>
2020-10-17 10:58:50 +0700

    Fixed minor bug

--- src/webhook.ts
+++ src/webhook.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 
 /**
