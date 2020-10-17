03f91e7040fa3fe7d28a9738b53ba724389efa9f
PhanKiet <tuankietvn@outlook.com>
2020-10-17 11:22:27 +0700

    update webhook method

--- dist/webhook.js
+++ dist/webhook.js
@@ -4,15 +4,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = express_1.default();
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
+/**
+ * @description Initialize the server and handle all the requests.
+ */
 const WEBHOOK = () => {
+    app.use(body_parser_1.default.json());
+    app.use(express_1.default.urlencoded({ extended: false }));
     app.get("/", (req, res) => {
-        res.send("bruh");
+        res.send("Bruh");
     });
-    app.listen(port, () => {
-        return console.log(`Listening on ${port}`);
+    app.get('/verifyWebhook', (req, res) => {
+        const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+        const mode = req.query['hub.mode'];
+        const token = req.query['hub.verify_token'];
+        const challenge = req.query['hub.challenge'];
+        if (mode && token) {
+            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+                console.log('VERIFIED');
+                res.status(200).send(challenge);
+            }
+            else
+                res.sendStatus(403);
+        }
     });
+    // Start the server
+    app.listen(port, () => console.log(`Listening on ${port}`));
 };
 exports.WEBHOOK = WEBHOOK;
 //# sourceMappingURL=webhook.js.map
