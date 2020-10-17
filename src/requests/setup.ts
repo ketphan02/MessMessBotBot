2c8c47f90cff0687ca6031ddace558839375e53c
PhanKiet <tuankietvn@outlook.com>
2020-10-17 16:26:22 +0700

    solving messenger's problem

--- src/requests/setup.ts
+++ src/requests/setup.ts
@@ -8,7 +8,7 @@ import { GetStartedButton } from '../utils/persistent_menu';
  */
 export default function setup(app: express.Express)
 {
-    app.get("/setup", (req: Request, res: Response) =>
+    app.post("/setup", (req: Request, res: Response) =>
     {
         GetStartedButton(res);
     })
