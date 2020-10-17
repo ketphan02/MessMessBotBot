2f15236af241d0a3cc53151d803391d9ad079643
PhanKiet <tuankietvn@outlook.com>
2020-10-17 10:32:33 +0700

    Connecting Heroku

--- src/webhook.ts
+++ src/webhook.ts
@@ -18,6 +18,25 @@ const WEBHOOK = () =>
         res.send("Bruh");
     });
 
+    app.get('/verfyWebhook', (req, res) =>
+    {
+        const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+        const mode = req.query['hub.mode'];
+        const token = req.query['hub.verify_token'];
+        const challenge = req.query['hub.challenge'];
+
+        if (mode && token)
+        {
+            if (mode === 'subscribe' && token === VERIFY_TOKEN)
+            {
+                console.log('VERIFIED');
+                res.status(200).send(challenge);
+            }
+            else res.sendStatus(403);
+        }
+    });
+
     // Start the server
     app.listen(port, () => console.log(`Listening on ${port}`));
 }
