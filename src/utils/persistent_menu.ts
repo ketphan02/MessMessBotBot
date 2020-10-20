97e777d9707b1bbef905f2d0d9beee308f9b7920
PhanKiet <tuankietvn@outlook.com>
2020-10-20 11:17:01 +0700

    create starting menu

--- src/utils/persistent_menu.ts
+++ src/utils/persistent_menu.ts
@@ -55,7 +55,7 @@ const StartingMenu = (res: express.Response) =>
 
     request(
     {
-        "url": `https://graph.facebook.com/v2.6/me/thread_settings?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
+        "url": `https://graph.facebook.com/v2.6/me/messenger_profile?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
         "method": "POST",
         "headers":
         {
