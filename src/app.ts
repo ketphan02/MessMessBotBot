6ff830b9c07046cec8cb9fef3acb6cafa5686387
PhanKiet <tuankietvn@outlook.com>
2020-10-20 20:45:14 +0700

    keeping heroku alive

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import WEBHOOK from './webhook';
 
+import axios from 'axios';
+
 async function __main__()
 {
     WEBHOOK();
+    await keepAlive();
 }
+
+async function keepAlive()
+{
+    setInterval(() =>
+    {
+        const url = `https://${process.env.APP_NAME}.herokuapp.com`;
+
+        axios.get(url);
+    }, 19 * 60 * 1000);
+}
+
 
 __main__();
