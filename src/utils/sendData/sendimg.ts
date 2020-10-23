8a7a264a39ada8b57374a274927b1ce5d5b47ecc
PhanKiet <tuankietvn@outlook.com>
2020-10-24 00:33:54 +0700

    update

--- src/utils/sendData/sendimg.ts
+++ src/utils/sendData/sendimg.ts
@@ -23,16 +23,16 @@ export default async function sendImage(sender_id: String, imglink: String)
     }
 
     request(
-    {
-        url: `https://graph.facebook.com/v${process.env.FB_GRAPH_API_VERSION}/me/messages`,
-        qs:
+        {
+        "url": `https://graph.facebook.com/v${process.env.FB_GRAPH_API_VERSION}/me/messages`,
+        "qs":
         {
             "access_token": process.env.PAGE_ACCESS_TOKEN
         },
-        method: "POST",
-        json: data
+        "method": "POST",
+        "json": data
     }, (error, response, body) =>
     {
-        if (error) console.log(`Unable to send img: ${error}`);
+        if (error) console.log(`Unable to send message: ${error}`);
     });
 }
